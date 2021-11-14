53c30a7ec45070fc5621ebd043111fe97d9a9c93
luisrfpam <luisrfp.am@gmail.com>
2021-11-14 19:49:31 -0300

    H2 database

--- backend/src/main/java/com/devsuperior/dscatalog/resources/CategoryResource.java
+++ backend/src/main/java/com/devsuperior/dscatalog/resources/CategoryResource.java
@@ -1,28 +1,33 @@
 package com.devsuperior.dscatalog.resources;
 
-import java.util.ArrayList;
 import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.devsuperior.dscatalog.entities.Category;
+import com.devsuperior.dscatalog.services.CategoryService;
 
 // Controlador REST que vai responder as requisições deste recurso
 @RestController // transforma esse recurso em REST
 @RequestMapping(value = "/categories") // rota REST do seu recurso - a rota
 public class CategoryResource {
 	
+	@Autowired
+	private CategoryService service;
+	
 	// Criação de um Endpoint - a primeira rota possível que irá responder a requisição
 	// ResponseEntity é um objeto do spring que ele encapsula uma resposta HTTP 
 	@GetMapping
 	public ResponseEntity<List<Category>> findAll(){
 		
-		List<Category> list = new ArrayList<Category>();
-		list.add(new Category(1L, "Books"));
-		list.add(new Category(2L, "Eletronics"));
+		List<Category> list = service.findAll();
+		//List<Category> list = new ArrayList<Category>();
+		//list.add(new Category(1L, "Books"));
+		//list.add(new Category(2L, "Eletronics"));
 		return ResponseEntity.ok().body(list);
 		
 	}
